77a241fbe8eb20b3a38faec4e30ceadb1c8cca56
Gabriel Faria <gabrielfaria88@gmail.com>
2024-04-24 10:30:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'The Lord of The Rings',
+      url: 'https://lotr.org/',
+      author: 'J.R.R. Tolkien',
+      num_comments: 10,
+      points: 5,
+      objectID: 2,
+    },
+    {
+      title: 'Manifesto Comunista',
+      url: 'https://marxists.org/',
+      author: 'Karl Marx, Friedrich Engels',
+      num_comments: 2,
+      points: 5,
+      objectID: 3,
+    },
+  ];
+
+const title = 'you'
+
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+}
+
+function getTitle(title) {
+  return title;
+}
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        My Hacker Stories
+      </h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+
+      <hr />
+
+      {list.map(function(item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> - {item.author}, </span>
+            <span>{item.num_comments}, </span>
+            <span>{item.points}</span>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
